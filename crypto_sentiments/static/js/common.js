7ea3d2253a04a7b5b6ea88a9449f84e719e60cb8
Charles Wan <yty.charles@gmail.com>
2017-11-28 20:56:27 -0500

    Add footer to all pages including link to github repo

--- crypto_sentiments/static/js/common.js
+++ crypto_sentiments/static/js/common.js
@@ -2,12 +2,15 @@
 // Common js file for all pages (for things like navbar)
 
 $(document).ready(function() {
-    // menu
-    $("#navbar-container").load("/static/html/navbar.html", function() {
+    // navbar
+    $("header").load("/static/html/navbar.html", function() {
         $("#navbarNav a").each(function() {
             if ($(this).attr("href") === window.location.pathname) {
                 $(this).parent().addClass("active");
             }
         });
     });
+
+    // footer
+    $("footer").load("/static/html/footer.html");
 });
